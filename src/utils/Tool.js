6bb64c714e6e4e000359629fb5acfe0620771334
leiyu <927049198@qq.com>
2025-12-09 11:36:46 +0800

    1209

--- src/utils/Tool.js
+++ src/utils/Tool.js
@@ -1,28 +1,13 @@
-function getValue(data, key) {
-  return data?.[key]?.bindValue || ''
-}
-
-function getId(data, key) {
-  return data?.[key]?.id || ''
-}
-
-
-// 添加节流工具函数
-const throttle = (func, limit) => {
-  let inThrottle;
-  return function () {
-    const args = arguments;
-    const context = this;
-    if (!inThrottle) {
-      func.apply(context, args);
-      inThrottle = true;
-      setTimeout(() => inThrottle = false, limit);
-    }
-  }
+// 判断是否是手机端
+const isMobile = () => {
+  const userAgent = navigator.userAgent;
+  const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return mobileRegex.test(userAgent);
 };
 
 export {
   getId,
   getValue,
-  throttle
+  throttle,
+  isMobile
 }
